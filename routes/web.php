0f22e3e999c28f871564f89846a1be95f1e78e8b
Paulo Valente Maia <paulovalentemaia>
2022-04-02 18:32:03 -0300

    Blade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = "Paulo";
+    $idade = 29;
+    $arr = [10,20,30,40,50];
+    $nomes = ['Paulo', 'Luziete', 'Arthur', 'Kayo', 'Apólo', 'Thor'];
+
+    return view('welcome',
+        [
+            'nome' => $nome,
+            'idade2' => $idade,
+            'profissao' => 'Programador',
+            'arr' => $arr,
+            'nomes' => $nomes
+        ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+    return view('products');
 });
