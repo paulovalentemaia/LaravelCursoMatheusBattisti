7bbbda6fdd351b221f7c3a7bd6a4ee7a04c89b48
Paulo Valente Maia <paulovalentemaia@gmail.com>
2022-04-06 14:25:58 -0300

    Salvando dados no bd json - Alterando Model $casts

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -27,6 +27,25 @@
                 </select>
             </div>
             <div class="form-group">
+                <label for="title">Adicione itens de infraestrutura:</label>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Palco"> Palco
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Cerveja Grátis"> Cerveja Grátis
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Open Food"> Open Food
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Brindes"> Brindes
+                </div>
+
+            </div>
+            <div class="form-group">
                 <label for="title">Descrição:</label>
                 <textarea name="description" id="description" placeholder="O que vai acontecer no evento?"
                           class="form-control"></textarea>
