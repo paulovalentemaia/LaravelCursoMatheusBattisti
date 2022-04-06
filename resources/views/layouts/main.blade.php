fa2b43b614420863569fd18dd3286125280a804c
Paulo Valente Maia <paulovalentemaia>
2022-04-06 20:29:07 -0300

    Autenticação, alterando links se guest ou auth

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -31,14 +31,33 @@
                     <a href="/" class="nav-link">Eventos</a>
                 </li>
                 <li class="nav-item">
-                    <a href="/events/create" class="nav-link">Criar Evento</a>
+                    <a href="/events/create" class="nav-link">Criar Eventos</a>
                 </li>
-                <li class="nav-item">
-                    <a href="/" class="nav-link">Entrar</a>
-                </li>
-                <li class="nav-item">
-                    <a href="/" class="nav-link">Cadastrar</a>
-                </li>
+                @auth
+                    <li class="nav-item">
+                        <a href="/dashboard" class="nav-link">Meus Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                        <form action="/logout" method="post">
+                            @csrf
+                            <a href="/logout"
+                               class="nav-link"
+                               onclick="event.preventDefault();
+                                    this.closest('form').submit();"
+                            >
+                            Sair
+                            </a>
+                        </form>
+                    </li>
+                @endauth
+                @guest
+                    <li class="nav-item">
+                        <a href="/login" class="nav-link">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/register" class="nav-link">Cadastrar</a>
+                    </li>
+                @endguest
             </ul>
         </div>
     </nav>
