e849e718c8f8ab03eb2cae1ce651337fba4195fa
Paulo Valente Maia <paulovalentemaia>
2022-04-07 16:32:22 -0300

    removendo presença do usuario

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -32,10 +32,23 @@ class EventController extends Controller
     {
         $event = Event::findOrFail($id);
 
+        //Verifica se já tem presença
+        $user = auth()->user();
+        $hasUserJoined = false;
+        if ($user) {
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach ($userEvents as $userEvent) {
+                if ($userEvent['id'] == $id) {
+                    $hasUserJoined = true;
+                }
+            }
+        }
+
         //Pegando dado de outra tabela
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
     }
 
     public function store(Request $request)
@@ -128,5 +141,16 @@ class EventController extends Controller
         $event = Event::findOrFail($id);
 
         return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento ' . $event->title);
+    }
+
+    public function leaveEvent($id)
+    {
+
+        $user = auth()->user();
+        $user->eventsAsParticipant()->detach($id);
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Você saiu com sucesso do evento ' . $event->title);
+
     }
 }
